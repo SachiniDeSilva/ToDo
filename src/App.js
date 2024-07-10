dc238a100ddd75a0fea047783c3e3b2ec2ad056f
Williyam Silvage Sachini Dishala <90495152+desi98@users.noreply.github.com>
2024-07-11 00:31:29 +0530

    start todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+
+import React, { useState } from 'react';
+
 import './App.css';
+import { AiOutlineDelete } from 'react-icons/ai';
+import { BsCheck } from 'react-icons/bs';
 
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="content">
+        <h1>My ToDo</h1>
+
+        <div className="todo-wrapper">
+          <div className="todo-input">
+            <div className="todo-input-item">
+              <label>Title</label>
+              <input type="text" placeholder="What is the task Title" />
+            </div>
+
+            <div className="todo-input-item">
+              <label>Description</label>
+              <input type="text" placeholder="What is the task Description" />
+            </div>
+
+            <div className="todo-input-item">
+              <button type="button" className="primaryBtn">Add</button>
+            </div>
+          </div>
+
+          <div className="btn-area">
+            <button
+              className={`seconBtn ${!isCompleteScreen ? 'active' : ''}`}
+              onClick={() => setIsCompleteScreen(false)}
+            >
+              Todo
+            </button>
+            <button
+              className={`seconBtn ${isCompleteScreen ? 'active' : ''}`}
+              onClick={() => setIsCompleteScreen(true)}
+            >
+              Completed
+            </button>
+          </div>
+
+          <div className="todo-list">
+            <div className="todo-list-item">
+              <h3>Task 1</h3>
+              <p>Description</p>
+            </div>
+            <div>
+              <AiOutlineDelete className='icon'/>
+              <BsCheck className='check-icon'/>
+       
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
